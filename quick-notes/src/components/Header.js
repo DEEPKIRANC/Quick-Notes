8e4509887e408271679d301af4351b3d000365ca
Deep Kiran <cdeepkiran14@gmail.com>
2021-04-20 16:55:11 +0530

    Add Header Component

---
+++ quick-notes/src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import "../styles/header.css";
+
+function Header() {
+    return (
+        <div className="Navbar"> 
+            <h1>QuickNotes</h1>
+        </div>
+    )
+}
+
+export default Header
