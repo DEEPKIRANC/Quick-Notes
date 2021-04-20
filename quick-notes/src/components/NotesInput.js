997895ab1faabcf370f7cb7740b4f56c51c9b284
Deep Kiran <cdeepkiran14@gmail.com>
2021-04-21 03:42:35 +0530

    Add Styling

--- quick-notes/src/components/NotesInput.js
+++ quick-notes/src/components/NotesInput.js
@@ -1,9 +1,11 @@
-import React from 'react'
+import React,{useState} from 'react'
 import "../styles/notesinput.css";
 import DoneAllIcon from '@material-ui/icons/DoneAll';
 
 
 function NotesInput() {
+    const [title,setTitle]=useState("");
+    const [note,setNote]=useState("");
     return (
         <div className="inputsection">
             <div className="overview">
@@ -12,9 +14,19 @@ function NotesInput() {
                 <span><DoneAllIcon />Use Our Interactive Editor to make changes in realtime</span>
                 <span><DoneAllIcon />Bookmark your Important notes </span> 
                 <span><DoneAllIcon />Arrange your notes based on time of creation / updation</span> 
+                <span><DoneAllIcon />Pick a background color for your Note</span>
                 <span><DoneAllIcon />Delete a Note</span>   
             </div>
-            <div className="noteinput">Input section of this app</div>            
+            <div className="noteinput">
+                <h3>Create New Note</h3>
+                <form>
+                    <div className="inputbox">
+                        <input type="text" className="inputs" value={title} placeholder="Add Title" onChange={e=>setTitle(e.target.value)}  />
+                        <input type="text" className="inputs" value={note} placeholder="Write Something" onChange={e=>setNote(e.target.value)} />
+                        <button type="submit">Submit Note</button>
+                    </div>
+                </form>        
+            </div>            
         </div>
     )
 }
